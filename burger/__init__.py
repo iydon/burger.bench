1a05b44ade66fb75dca5971b349e7992a20f5cac
Iydon Liang <liangiydon@gmail.com>
2022-07-04 10:39:14 +0800

    :building_construction: Adapted to current template architecture

--- burger/__init__.py
+++ burger/__init__.py
@@ -13,9 +13,9 @@ from .lang import *
 
 class Benches:
     Ts = {
-        key[5:].lower(): value
-        for key, value in globals().items()
-        if key.startswith('Bench') and key[5:] and key[5].isupper()
+        key[5:].lower(): values
+        for key, values in globals().items()
+        if key.startswith('Bench') and key[5:] and key[5].isupper() and values
     }
 
     def __init__(
@@ -26,53 +26,53 @@ class Benches:
     ) -> None:
         directory = p.Path(directory)
         self.benches = {
-            key: Value(directory/key, N, RE, L, T, CFL)
-            for key, Value in self.Ts.items()
+            lang: {
+                name: Bench(directory/lang, N, RE, L, T, CFL)
+                for name, Bench in Benches.items()
+            } for lang, Benches in self.Ts.items()
         }
 
-    def __getitem__(self, key: str) -> Bench:
-        return self.benches[key.lower()]
+    def __getitem__(self, lang: str) -> t.Dict[str, Bench]:
+        return self.benches[lang.lower()]
 
     @classmethod
     def version(self) -> t.Dict[str, str]:
         return {
-            key: value.version()
-            for key, value in self.Ts.items()
+            lang: next(iter(benches.values())).version()
+            for lang, benches in self.Ts.items()
         }
 
     @property
-    def result(self) -> t.Dict[str, t.Optional[np.ndarray]]:
-        return {
-            key: value.result
-            for key, value in self.benches.items()
-        }
+    def result(self) -> t.Dict[str, t.Dict[str, t.Optional[np.ndarray]]]:
+        return self._map(lambda lang, name, bench: bench.result)
 
     def compile(self) -> 'Benches':
-        for bench in self.benches.values():
-            bench.compile()
+        self._map(lambda lang, name, bench: bench.compile())
         return self
 
     def run(self) -> 'Benches':
-        for bench in self.benches.values():
-            tic = time.time()
-            bench.run()
-            toc = time.time()
-            print(f'[{bench}] Elapsed time is {toc-tic} seconds.')
+        for benches in self.benches.values():
+            for bench in benches.values():
+                tic = time.time()
+                bench.run()
+                toc = time.time()
+                print(f'[{bench}] Elapsed time is {toc-tic} seconds.')
         return self
 
     def reset(self) -> 'Benches':
-        for bench in self.benches.values():
-            bench.reset()
+        self._map(lambda lang, name, bench: bench.reset())
         return self
 
-    def hyperfine(self, warmup: int = 3, min_runs: int = 9) -> t.Dict[str, t.Dict[str, t.Any]]:
-        return {
-            key: value.hyperfine(warmup, min_runs)
-            for key, value in self.benches.items()
-        }
+    def hyperfine(self, warmup: int = 3, min_runs: int = 9, max_runs: int = 16) -> t.Dict[str, t.Dict[str, t.Dict[str, t.Any]]]:
+        return self._map(lambda lang, name, bench: bench.hyperfine(warmup, min_runs, max_runs))
 
-    def memory(self, milliseconds: int = 5) -> t.Dict[str, t.Dict[str, float]]:
+    def memory(self, milliseconds: int = 5) -> t.Dict[str, t.Dict[str, t.Dict[str, float]]]:
+        return self._map(lambda lang, name, bench: bench.memory(milliseconds))
+
+    def _map(self, func: t.Callable) -> t.Dict[str, t.Dict[str, t.Any]]:
         return {
-            key: value.memory(milliseconds)
-            for key, value in self.benches.items()
+            lang: {
+                name: func(lang, name, bench)
+                for name, bench in benches.items()
+            } for lang, benches in self.benches.items()
         }
